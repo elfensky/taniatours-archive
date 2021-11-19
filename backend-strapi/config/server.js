c0093b8ddbd70efec3ee97d7a98566ba2d2e5487
Andrei Lavrenov <andrei@lavrenov.io>
2021-11-19 22:55:28 +0100

    ran docker-compose for strapi
    The necessary files and initial configuration to run a strapi backend for the main website.
    This will be used to create the content of the website like the text for various pages, descriptions, tours etc...

---
+++ backend-strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3e5fd822fc8ad6c412390143357f539e'),
+    },
+  },
+});
---
+++ backend-strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '90f0a30d-cb51-42b0-9785-59088b1eba44'
+};
