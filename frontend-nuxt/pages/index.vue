2933f75f619dd44e28413a4604735fa7b797e974
Andrei Lavrenov <andrei@lavrenov.io>
2021-11-20 20:32:13 +0100

    created a nuxt project
    created a new empty nuxt project

---
+++ frontend-nuxt/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+	<Tutorial />
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({});
+</script>
+
+<style>
+h2 {
+	background-color: red;
+}
+</style>
